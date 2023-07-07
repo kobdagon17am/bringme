a9c602e524232e36ffdf29dbf6b650dec73ef00d
Sasanapong <sassanapong.p@gmail.com>
2023-07-07 17:42:08 +0700

    dddd

--- local/resources/views/layouts/Admin/app.blade.php
+++ local/resources/views/layouts/Admin/app.blade.php
@@ -6,7 +6,7 @@
     <head>
         <head>
             <meta charset="utf-8">
-            <link href="dist/images/logo.svg" rel="shortcut icon">
+            <link href="{{asset('admin_st/dist/images/logo.svg')}}" rel="shortcut icon">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <meta name="description" content="Enigma admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
             <meta name="keywords" content="admin template, Enigma Admin Template, dashboard template, flat admin template, responsive admin template, web app">
@@ -17,16 +17,17 @@
 
             <link rel="stylesheet" href="{{asset('admin_st/dist/css/app.css')}}" />
 
-            <script src="{{asset('admin_st/dist/js/app.js')}}" defer></script>
+            {{-- <script src="{{asset('admin_st/dist/js/app.js')}}" defer></script> --}}
             <link href='https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css' rel='stylesheet'>
+
             <!-- END: CSS Assets-->
 
             <!-- BEGIN: JS Assets-->
-            <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 
-            <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
-            <script src="https://maps.googleapis.com/maps/api/js?key=[" your-google-map-api"]&libraries=places"></script>
-            <script src="{{asset('admin_st/dist/js/ckeditor-classic.js')}}"></script>
+
+            <script defer src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
+            <script defer src="https://maps.googleapis.com/maps/api/js?key=[" your-google-map-api"]&libraries=places"></script>
+            <script defer src="{{asset('admin_st/dist/js/ckeditor-classic.js')}}"></script>
 
 
             <!-- END: JS Assets-->
@@ -52,6 +53,13 @@
 
             <!-- END: Content -->
         </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>
 
         <script>
             $(document).ready(function() {
