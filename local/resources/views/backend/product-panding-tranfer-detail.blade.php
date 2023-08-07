0445fb3575305c40b10c63635847124c10be0966
Sasanapong <sassanapong.p@gmail.com>
2023-08-07 15:44:19 +0700

    รายละเอีดสินค้า

--- local/resources/views/backend/product-panding-tranfer-detail.blade.php
+++ local/resources/views/backend/product-panding-tranfer-detail.blade.php
@@ -26,7 +26,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <input id="brand-name" type="text" class="form-control" placeholder="Brand name">
+                                <input id="brand-name" type="text" class="form-control" placeholder="Brand name" value="TH: Orange and bread">
                             </div>
                             <div class="form-inline items-start flex-col xl:flex-row mt-5 pt-5 first:mt-0 first:pt-0">
                                 <div class="form-label xl:w-64 xl:!mr-10">
@@ -213,6 +213,53 @@
 
                                 </div>
 
+                             <div class="form-inline items-start flex-col xl:flex-row mt-5 pt-5 first:mt-0 first:pt-0">
+                                    <div class="form-label xl:w-64 xl:!mr-10">
+                                        <div class="text-left">
+                                            <div class="flex items-center">
+                                                <div class="font-medium">Tracking Number</div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                        <input  type="text" class="form-control w-56 block mx-auto" placeholder="Tracking Numbe">
+
+
+
+
+                                </div>
+
+                                <div class="form-inline items-start flex-col xl:flex-row mt-5 pt-5 first:mt-0 first:pt-0">
+                                    <div class="form-label xl:w-64 xl:!mr-10">
+                                        <div class="text-left">
+                                            <div class="flex items-center">
+                                                <div class="font-medium">ชื่อขนส่ง</div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <input  type="text" class="form-control w-56 block mx-auto" placeholder="ชื่อขนส่ง">
+
+
+                                </div>
+
+                                <div class="form-inline items-start flex-col xl:flex-row mt-5 pt-5 first:mt-0 first:pt-0">
+                                    <div class="form-label xl:w-64 xl:!mr-10">
+                                        <div class="text-left">
+                                            <div class="flex items-center">
+                                                <div class="font-medium">เบอร์โทรศัพท์</div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                        <input type="text" class="form-control w-56 " placeholder="เบอร์โทรศัพท์">
+
+
+
+
+                                </div>
 
                                 <div class="form-inline items-start flex-col xl:flex-row mt-5 pt-5 first:mt-0 first:pt-0">
                                     <div class="form-label xl:w-64 xl:!mr-10">
@@ -254,7 +301,7 @@
                                     <div class="form-label xl:w-64 xl:!mr-10">
                                         <div class="text-left">
                                             <div class="flex items-center">
-                                                <div class="font-medium">สลิปโอนเงิน</div>
+                                                <div class="font-medium">หลักฐานการจัดส่ง</div>
                                             </div>
                                         </div>
                                     </div>
@@ -328,6 +375,23 @@
 
                                             @endif
                                         </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-inline items-start flex-col xl:flex-row mt-5 pt-5 first:mt-0 first:pt-0">
+                                    <div class="form-label xl:w-64 xl:!mr-10">
+                                        <div class="text-left">
+                                            <div class="flex items-center">
+                                                <div class="font-medium">หมายเหตุ</div>
+                                                {{-- <div
+                                                    class="ml-2 px-2 py-0.5 bg-slate-200 text-slate-600 dark:bg-darkmode-300 dark:text-slate-400 text-xs rounded-md">
+                                                    Required</div> --}}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="w-full mt-3 xl:mt-0 flex-1">
+                                        <textarea class="form-control" placeholder="หมายเหตุ">
+                                        </textarea>
                                     </div>
                                 </div>
 
