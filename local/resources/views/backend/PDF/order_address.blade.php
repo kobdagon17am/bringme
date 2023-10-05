3e867585f394673c2a744b9855e6f731b0812461
Sasanapong <sassanapong.p@gmail.com>
2023-10-05 13:42:28 +0700

    bacode

---
+++ local/resources/views/backend/PDF/order_address.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<?php
+// dd(asset('frontend/dist/fonts/THSarabunNew.ttf'));
+?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="{{ asset('frontend/dist/css/font.css')}}" rel="stylesheet" type="text/css" />
+    <style>
+        body,th,tr,table,td{
+            font-family: 'THSarabunNew,Arial,sans-serif';
+            font-size: 14px;
+            font-weight: normal;
+            /* line-height: 14px;
+            margin-top: 0px;
+            margin-left: 13px;
+            margin-right: 33px;
+            line-height: 13px; */
+        }
+        b,h5 {
+            font-family: 'THSarabunNew-b,Arial,sans-serif';
+            font-size: 16px;
+
+        }
+        @page {
+            padding: 10px;
+            size: 100mm 150mm;
+            margin: 10px; /* Set margins to 0 if you want no margins */
+        }
+
+
+
+    </style>
+</head>
+
+<body>
+    <p style="text-align: end">Tracking No. MWE15230000072811</p>
+    <table style="width: 100%;border: 1px;color: black;" border="2">
+        <thead>
+            <tr>
+                <th align="left" style="padding-left: 5px">
+                    <p>ชื่อผู้ส่ง(Sender)<br>
+                   ชื่อ นิติพล เล่าเปี่ยม Moomike Shop <br>
+                    โทร 078-123-4567 โทร 087-676-5394 <br>
+                    ที่อยู่ 600 ซอยป้าไม้4 สุขุมวิท77/1 พระโขนง  <br>
+                    คลองเตย กรุงเทพมหานคร <br>
+                    11130</p>
+                </th>
+                <th align="left" style="padding-left: 5px">
+
+                    <p>
+                        ผู้รับ(Receiver)<br>
+                        ชื่อ นิติพล เล่าเปี่ยม Moomike Shop <br>
+                        โทร 078-123-4567 โทร 087-676-5394 <br>
+                        ที่อยู่ 600 ซอยป้าไม้4 สุขุมวิท77/1 พระโขนง  <br>
+                        คลองเตย กรุงเทพมหานคร <br>
+                        11130</p>
+                </th>
+            </tr>
+            <tr>
+                <th align="left" style="padding-left: 2px">
+                    <p> เลขที่ใบสั่งซื้อ 0420200000014 <br>
+                        วันที่สั่งซื้อ 17 Apr 2020 16:40 <br>
+                        วิธีการจัดส่ง Flash </p>
+                 </th>
+                 <th style="">
+                    <p>การชำระเงิน<br>
+                    โอนเงินผ่านบัญชีธนาคาร<br>
+                        (ชำระเงินแล้ว)
+                     </p>
+                 </th>
+            </tr>
+        </thead>
+    </table>
+    <p>*หากมีปัญหาเกี่ยวกับสินค้า กรุณาติตต่อบริษัทที่ท่านสั่งซื้อสินคำโดยตรง และหากหัสดุดีกลับกรุณานำส่งตามชื่อผู้ฝากส่งรายการสินค้า</p>
+
+</body>
+<html>
