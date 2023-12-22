7236c21339f9259956bbcaa8dc46c06ae52aaad4
sassanapong17am <sassanapong.p@gmail.com>
2023-12-22 14:19:48 +0700

    dddd

--- local/routes/api.php
+++ local/routes/api.php
@@ -101,8 +101,10 @@ Route::post('api_add_withdraw', 'API3Controller@api_add_withdraw');
 Route::post('api_add_brands', 'API3Controller@api_add_brands');
 Route::get('api_re_db', 'API3Controller@api_re_db');
 
-Route::get('label/print/{cart_id}', 'Admin\OrdersController@order_print_api')->name('label/print');
+
 Route::post('api_send_message', 'API3Controller@api_send_message');
 Route::post('api_get_message', 'API3Controller@api_get_message');
 Route::post('api_get_message_list', 'API3Controller@api_get_message_list');
 Route::post('api_send_image', 'API3Controller@api_send_image');
+
+
